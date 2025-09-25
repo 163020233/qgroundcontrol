eb643592971d2c515b9113d71445875907caab53
你ç163020233 <1497098056@qq..com>
2025-09-25 14:48:02 +0800

    [添加] 1.设置openstreet地图的种类

--- src/QtLocationPlugin/QGCMapUrlEngine.cpp
+++ src/QtLocationPlugin/QGCMapUrlEngine.cpp
@@ -26,7 +26,9 @@
 QGC_LOGGING_CATEGORY(QGCMapUrlEngineLog, "qgc.qtlocationplugin.qgcmapurlengine")
 
 const QList<SharedMapProvider> UrlFactory::_providers = {
-    std::make_shared<OpenStreetMapProvider>(),
+    std::make_shared<OSMStreetMapProvider>(),
+    std::make_shared<OSMTerrainMapProvider>(),
+    std::make_shared<OSMHybridMapProvider>(),
 #ifndef QGC_NO_GOOGLE_MAPS
     std::make_shared<GoogleStreetMapProvider>(),
     std::make_shared<GoogleSatelliteMapProvider>(),
